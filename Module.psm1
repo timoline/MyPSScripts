592212099defa6d51f9ba611d90614b825f71a97
timoline <timoline@gmail.com>
2019-03-31 09:34:14 +0200

    added psm1 template

---
+++ Module.psm1
@@ -0,0 +1,20 @@
+# region Load of module functions 
+$PublicFunctions = @( Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -ErrorAction SilentlyContinue )
+$PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -ErrorAction SilentlyContinue )
+
+# Load the separate function files from the private and public folders.
+$AllFunctions = $PublicFunctions + $PrivateFunctions
+foreach ($function in $AllFunctions)
+{
+    try
+    {
+        . $function.Fullname
+    }
+    catch
+    {
+        Write-Error -Message "Failed to import function $($function.fullname): $_"
+    }
+}
+
+# Export the public functions
+Export-ModuleMember -Function $PublicFunctions.BaseName
