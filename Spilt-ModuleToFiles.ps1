a71991dd90a6af42548c965490931c47e3b26493
timoline <timoline@gmail.com>
2019-03-31 09:30:38 +0200

    added First script

---
+++ Spilt-ModuleToFiles.ps1
@@ -0,0 +1,25 @@
+$ModuleName = "PlatyPS"
+$Module = "" #Specify the module 
+$SplittedFunctionPath = "" #Specify Splitted function path
+
+#Import-Module
+Import-Module $Module -Force -Verbose
+
+#Function to split the module and export it as functions
+Function Insert-Content 
+{
+    param ( [String]$Path )
+    process 
+    {
+        $( , $_; Get-Content $Path -ea SilentlyContinue) | Out-File $Path
+    }
+}
+$FunctionName = Get-Command -Module $ModuleName 
+$path = "$SplittedFunctionPath" 
+Foreach ($Function in $FunctionName.Name)
+{
+    (Get-Command $Function).definition | Out-File $Path\$Function.ps1
+    "Function $Function `n {" | Insert-Content $Path\$Function.ps1
+    "}" | Add-Content $Path\$Function.ps1
+
+}
